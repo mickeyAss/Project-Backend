802d3ba0a7c577848eebea3906c6101866f9e761
micsarawut <65011212206@msu.ac.th>
2024-03-21 00:35:55 +0700

    ddd

--- api/upload.ts
+++ api/upload.ts
@@ -46,36 +46,28 @@ class FileMiddleware {
 // POST /upload
 const fileUpload = new FileMiddleware();
 router.post("/", fileUpload.diskLoader.single("file"), async (req, res) => {
-  try {
-    const filename = Date.now() + "-" + Math.round(Math.random() * 10000) + ".png";
 
-    // Define saving filename on Firebase storage
-    const storageRef = ref(storage, "/images/" + filename);
-    
-    // Define file metadata
-    const metadata = {
-      contentType: req.file!.mimetype
-    };
+  const filename = Date.now() + "-" + Math.round(Math.random() * 10000) + ".png";
 
-    // Upload to Firebase storage
-    const snapshot = await uploadBytesResumable(storageRef, req.file!.buffer, metadata);
+  // define saving filename on firebase storage
+  const storageRef = ref(storage, "/images/" + filename);
+  //define file detail
 
-    // Get download URL
-    const url = await getDownloadURL(snapshot.ref);
-
-    // Insert URL into MySQL database
-    conn.query("INSERT INTO bigbike (bimg) VALUES (?)", [url], (err, result) => {
-      if (err) {
-        console.error("Error inserting data:", err);
-        res.status(500).json({ error: "Error inserting data" });
-      } else {
-        res.status(200).json({ url });
-      }
-    });
-  } catch (error) {
-    console.error("Error uploading image:", error);
-    res.status(500).json({ error: "Error uploading image" });
+  const metadata = {
+    contentType: req.file!.mimetype
   }
+
+  //upload to firebase storage
+  const snapshot = await uploadBytesResumable(
+    storageRef,
+    req.file!.buffer,
+    metadata);
+
+  //Return
+  const url = await getDownloadURL(snapshot.ref);
+  res.status(200).json({
+    file: url
+  });
 });
 
 router.post("/insert", (req, res) => {
