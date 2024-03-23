cb7cdceb8340ced2ed2fba16b58e225f4e2e6615
micsarawut <65011212206@msu.ac.th>
2024-03-23 17:49:34 +0700

    ww

--- api/imgrandom.ts
+++ api/imgrandom.ts
@@ -3,6 +3,19 @@ import { conn } from "../dbconnect";
 export const router = express.Router();
 
 
+router.get("/getBB/:bid", (req, res) => {
+  const bid = req.params.bid;
+  conn.query("SELECT * FROM `bigbike` WHERE bid = ?", [bid], (err, result) => {
+    if (err) {
+      console.error("Error retrieving BigBike data:", err);
+      res.status(500).json({ error: "Error retrieving BigBike data" });
+      return;
+    }
+    res.json(result);
+  });
+});
+
+
 // insert คะแนนที่เพิ่ม-ลดของแต่ละ bid ลง table vote
 router.post("/vote", (req, res) => {
   const { uid_fk, bid_fk, score, date } = req.body; // รับไอดีของรูปภาพและคะแนนจากข้อมูลที่ส่งมา
